4c91cfd3e91b90f9e98b562216a83d6e92af3282
Giosue1994 <giosuelentini1994@outlook.com>
2020-07-01 11:17:06 +0200

    search bar html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+  // 13feba4bc2e448e7522d3eaca8987928
+  // https://api.themoviedb.org/3/search/movie?api_key=13feba4bc2e448e7522d3eaca8987928&query=ritorno al futuro&language=it-IT
+
+});
