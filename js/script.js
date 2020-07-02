229b0d6bd6fb677a0fbf66c84e9b39737c364d95
Giosue1994 <giosuelentini1994@outlook.com>
2020-07-02 16:36:42 +0200

    aggiungi le bandiere alla lingua

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
   $('#search').click(function() {
     var inputSearch = $('#input-search').val();
     filmSearch(inputSearch);
-    tvShowSearch(inputSearch);
   });
 
   // al click del tasto invia sull'input cerco i film
@@ -14,7 +13,6 @@ $(document).ready(function() {
       if (event.which === 13 || event.keyCode === 13) {
         var inputSearch = $('#input-search').val();
         filmSearch(inputSearch);
-        tvShowSearch(inputSearch);
       }
     });
 
@@ -27,7 +25,7 @@ function filmSearch(filmQuery) {
 
   $.ajax(
     {
-      url: 'https://api.themoviedb.org/3/search/movie',
+      url: 'https://api.themoviedb.org/3/search/multi',
       method: "GET",
       data : {
         api_key: '13feba4bc2e448e7522d3eaca8987928',
@@ -38,44 +36,23 @@ function filmSearch(filmQuery) {
       success: function(data) {
         var film = data.results;
 
+        // eseguo un ciclo for in per ciclare gli oggetti
+        for (var key in film) {
+          var thisFilm = film[key];
+
+          // se il media_type è uuale a tv aggiungo un valore title che è uguale a name
+          if (thisFilm.media_type == 'tv') {
+            var thisFilmTitle = thisFilm['title']=thisFilm.name;
+            var thisFilmOriginalTitle = thisFilm['original_title']=thisFilm.original_name;
+          };
+
+        }
+
         if (film.length > 0) {
           printFilm(film);
         } else {
           messageError('Scrivi qualcosa di sensato cretino...');
-        }
-      },
-
-      error: function() {
-        messageError('Ricerca non avvenuta, forse dovresti scrivere qulacosa, non ti pare?');
-      }
-
-    }
-  );
-}
-
-//////////////////////////////////////////////////////
-// FUNZIONE RICERCA SERIE TV
-function tvShowSearch(tvShowQuery) {
-
-  $.ajax(
-    {
-      url: 'https://api.themoviedb.org/3/search/tv',
-      method: "GET",
-      data : {
-        api_key: '13feba4bc2e448e7522d3eaca8987928',
-        language: 'it-IT',
-        query: tvShowQuery
-      },
-
-      success: function(data) {
-        var tvShow = data.results;
-
-        if (tvShow.length > 0) {
-          printTvShow(tvShow);
-        } else {
-          messageError('Scrivi qualcosa di sensato cretino...');
-        }
-
+        };
       },
 
       error: function() {
@@ -96,46 +73,26 @@ function printFilm(film) {
   var template = Handlebars.compile(source);
 
   for (var i = 0; i < film.length; i++) {
-
     var thisFilm = film[i];
 
-    var context = {
-      title: thisFilm.title,
-      originalTitle: thisFilm.original_title,
-      language: thisFilm.original_language,
-      vote: thisFilm.vote_average,
-      tipology: 'Film'
+    if(thisFilm['media_type'] != 'person') {
+
+      var context = {
+        title: thisFilm.title,
+        originalTitle: thisFilm.original_title,
+        language: thisFilm.original_language,
+        flag: 'img/' + thisFilm.original_language + '.png',
+        vote: thisFilm.vote_average,
+        tipology: thisFilm.media_type
+      };
+
+      var html = template(context);
+
+      $('.film-list').append(html);
+    }
     };
 
-    var html = template(context);
 
-    $('.film-list').append(html);
-  }
-}
-
-//////////////////////////////////////////////////////
-// FUNZIONE STAMPA SERIE TV
-function printTvShow(tvShow) {
-
-  var source = $("#film-template").html();
-  var template = Handlebars.compile(source);
-
-  for (var i = 0; i < tvShow.length; i++) {
-
-    var thisTvShow = tvShow[i];
-
-    var context = {
-      title: thisTvShow.name,
-      originalTitle: thisTvShow.original_name,
-      language: thisTvShow.original_language,
-      vote: thisTvShow.vote_average,
-      tipology: 'Serie TV'
-    };
-
-    var html = template(context);
-
-    $('.film-list').append(html);
-  }
 }
 
 //////////////////////////////////////////////////////
