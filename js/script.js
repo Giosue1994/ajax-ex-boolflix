d5ed637b6d18d1e3a0d06eadc1b34ff8346ef902
Giosue1994 <giosuelentini1994@outlook.com>
2020-07-03 02:07:31 +0200

    modifica nomi variabili e aggiunta la funzione che stampa le immagini

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
-// 13feba4bc2e448e7522d3eaca8987928
-// https://api.themoviedb.org/3/search/movie?api_key=13feba4bc2e448e7522d3eaca8987928&query=ritorno al futuro&language=it-IT
 
-  // al click del bottone cerco i film che vengono scritti sull'input
+  // al click del bottone cerco i media che vengono scritti nell'input
   $('#search').click(function() {
     var inputSearch = $('#input-search').val();
-    filmSearch(inputSearch);
+    mediaSearch(inputSearch);
   });
 
-  // al click del tasto invia sull'input cerco i film
+  // al click del tasto invia nell'input cerco i media
   $('#input-search').keydown(function(event) {
       if (event.which === 13 || event.keyCode === 13) {
         var inputSearch = $('#input-search').val();
-        filmSearch(inputSearch);
+        mediaSearch(inputSearch);
       }
     });
 
@@ -20,8 +18,8 @@ $(document).ready(function() {
 
 
 //////////////////////////////////////////////////////
-// FUNZIONE RICERCA FILM
-function filmSearch(filmQuery) {
+// FUNZIONE RICERCA MEDIA
+function mediaSearch(mediaQuery) {
 
   // variabili ajax
   var url = 'https://api.themoviedb.org/3/search/multi';
@@ -35,26 +33,27 @@ function filmSearch(filmQuery) {
       data : {
         api_key: apiKey,
         language: language,
-        query: filmQuery
+        query: mediaQuery
       },
 
       success: function(data) {
-        var film = data.results;
+        var media = data.results;
 
         // eseguo un ciclo for in per ciclare gli oggetti
-        for (var key in film) {
-          var thisFilm = film[key];
+        for (var key in media) {
+          var thisMedia = media[key];
 
-          // se il media_type è uuale a tv aggiungo un valore title che è uguale a name
-          if (thisFilm.media_type == 'tv') {
-            var thisFilmTitle = thisFilm['title']=thisFilm.name;
-            var thisFilmOriginalTitle = thisFilm['original_title']=thisFilm.original_name;
+          // se il media_type è uguale a 'tv' aggiungo un valore 'title' che è uguale a 'name'
+          if (thisMedia.media_type == 'tv') {
+            var thisMediaTitle = thisMedia['title']=thisMedia.name;
+            var thisMediaOriginalTitle = thisMedia['original_title']=thisMedia.original_name;
           };
 
         }
 
-        if (film.length > 0) {
-          printFilm(film);
+        // se la lunghezza di quello che scrivo è maggiore di 0 stampo sullo schermo
+        if (media.length > 0) {
+          printMedia(media);
         } else {
           messageError('Scrivi qualcosa di sensato cretino...');
         };
@@ -70,32 +69,33 @@ function filmSearch(filmQuery) {
 
 
 //////////////////////////////////////////////////////
-// FUNZIONE STAMPA FILM
-function printFilm(film) {
+// FUNZIONE STAMPA MEDIA
+function printMedia(media) {
   reset();
 
-  var source = $("#film-template").html();
+  var source = $("#media-template").html();
   var template = Handlebars.compile(source);
 
-  for (var i = 0; i < film.length; i++) {
-    var thisFilm = film[i];
-    var poster = 'https://image.tmdb.org/t/p/w185' + thisFilm.poster_path;
+  for (var i = 0; i < media.length; i++) {
+    var thisMedia = media[i];
+    var poster = posterPath(thisMedia.poster_path);
+    console.log(poster);
 
-    // se il media_type è diverso da person stampa su schermo
-    if(thisFilm['media_type'] != 'person') {
+    // se il 'media_type' è diverso da 'person' stampa su schermo
+    if(thisMedia['media_type'] != 'person') {
 
       var context = {
         posterPath: poster,
-        title: thisFilm.title,
-        originalTitle: thisFilm.original_title,
-        language: thisFilm.original_language,
-        vote: voteInStar(thisFilm.vote_average),
-        tipology: thisFilm.media_type
+        title: thisMedia.title,
+        originalTitle: thisMedia.original_title,
+        language: thisMedia.original_language,
+        vote: voteInStar(thisMedia.vote_average),
+        tipology: thisMedia.media_type
       };
 
       var html = template(context);
 
-      $('.film-list').append(html);
+      $('.media-list').append(html);
     }
   };
 
@@ -120,9 +120,21 @@ function voteInStar(vote) {
 }
 
 //////////////////////////////////////////////////////
+// FUNZIONE IMMAGINE MOVIE
+function posterPath(poster) {
+  var posterPath = 'img/img_not_found.png';
+
+  if (poster != undefined) {
+    var posterPath = 'https://image.tmdb.org/t/p/w185' + poster;
+  }
+
+  return posterPath;
+}
+
+//////////////////////////////////////////////////////
 // FUNZIONE RESET
 function reset() {
-  $('.film-list').html('');
+  $('.media-list').html('');
 }
 
 //////////////////////////////////////////////////////
@@ -138,6 +150,6 @@ function messageError(message) {
 
   var html = template(context);
 
-  $('.film-list').append(html);
+  $('.media-list').append(html);
 
 }
